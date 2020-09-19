cb6840174aba0743aeb28ae96babb8049a7279e3
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-18 22:35:50 -0700

    made some edits

--- home-practice-helper.py
+++ home-practice-helper.py
@@ -5,31 +5,32 @@ def week_of_the_month(user_response):
    
 
 #add monthly practice by week
- bot_response_week_1 = ["downward dog", "forward bend", "savasana"] 
- bot_response_week_2 = ["downward dog", "back-bend" ,"savasana"]
- bot_response_week_3 = ["downward dog","tree pose", "twist", "savasana"]
- bot_response_week_4 = ["downward dog", "inversions", "savasana"]
+ bot_response_week_1 = ["downward dog", "uttanasana", "savasana"] 
+ bot_response_week_2 = ["locust", "back-bend on a chair" ,"upward dog"]
+ bot_response_week_3 = ["warrior 1","tree pose", "half moon", "chair pose"]
+ bot_response_week_4 = ["head stand", "shoulder stand", "head stand"]
 
 #more than one elif statement; each statement representing a possible input for the week of the month
- if user_response == "week1":
+ if user_response == "forward bends":
     return choice(bot_response_week_1)
- elif user_response == "week2":
+ elif user_response == "back bends":
     return choice(bot_response_week_2)
- elif user_response == "week3":
+ elif user_response == "standing poses":
     return random.choice(bot_response_week_3)
- elif user_response == "week4":
+ elif user_response == "inversions":
     return choice(bot_response_week_4)    
  else:
-    return "refer to: more practice ideas"   
+    return "For help with that pose, you should come to class! Let's practice together!"   
 
-print("Let's plan your practice!")
-print ("please enter the week of the month for a list of poses to practice this week.")
+print("What kind of poses would you like to practice at home today?")
+
+print ("Let me help you get started, you can type: forward bends, back bends, standing poses, inversions...")
 
 
 
 while True:
- user_response = input("Example: week1 week2 week3 week4: ")
- if user_response == "no":
+ user_response = input("Enter the type of pose you would like to practice: ")
+ if user_response == "stop":
   break
 
 
