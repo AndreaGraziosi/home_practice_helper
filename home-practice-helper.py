fe829dfa7dc02bd05f3055deac6a5804feb9ff47
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-19 14:53:44 -0700

    home-practice-helper.py

--- home-practice-helper.py
+++ home-practice-helper.py
@@ -1,14 +1,14 @@
 from random import choice
 import random
   
-def week_of_the_month(user_response):
+def get_bot_response(user_response):
    
 
 #add monthly practice by week
- bot_response_week_1 = ["downward dog", "uttanasana", "savasana"] 
- bot_response_week_2 = ["locust", "back-bend on a chair" ,"upward dog"]
- bot_response_week_3 = ["warrior 1","tree pose", "half moon", "chair pose"]
- bot_response_week_4 = ["head stand", "shoulder stand", "head stand"]
+ bot_response_week_1 = ["Downward dog for 20 minutes", "Forward bend sequence ending in parchimotanasana with these poses we hal the nervous system.", "1 hour long savasana, nothing better for the mind."] 
+ bot_response_week_2 = ["Start with a strong standing pose sequence", "Back-bend sequence on a chair" ,"Upward dog on a rope wall! Be  sure to work on arms before starting."]
+ bot_response_week_3 = ["10 sun salutations to get you started", "Tree pose for balance! Do all balancing poses you can remeber", "Incorporate twists today!"]
+ bot_response_week_4 = ["Hand stand sequence", "Shoulder stand and forearm balance! Yay it will be so fun!", "Hang on a sling for a few mintes and realign the spinal column!"]
 
 #more than one elif statement; each statement representing a possible input for the week of the month
  if user_response == "forward bends":
@@ -22,17 +22,17 @@ def week_of_the_month(user_response):
  else:
     return "For help with that pose, you should come to class! Let's practice together!"   
 
-print("What kind of poses would you like to practice at home today?")
+print("Namaste. Sit quietly in a comfortable position and chant om 3 times, notice the effect on your body.")
 
-print ("Let me help you get started, you can type: forward bends, back bends, standing poses, inversions...")
+print ("Let me help you get started with some ideas for poses today. You can type: forward bends, back bends, standing poses, inversions...")
 
 
 
 while True:
- user_response = input("Enter the type of pose you would like to practice: ")
- if user_response == "stop":
+ user_response = input("Enter the type of pose you would like to practice, and I can suggest a sequence! Om ")
+ if user_response == "done":
   break
 
 
- bot_response = week_of_the_month(user_response)
+ bot_response = get_bot_response(user_response)
  print(bot_response)
