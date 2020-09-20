a83498e84f09cc91bc92f61ef1e56defc5b9cff6
Andrea_Graziosi <andreaggraziosi@gmail.com>
2020-09-19 21:59:22 -0700

    final edits

--- home-practice-helper.py
+++ home-practice-helper.py
@@ -4,13 +4,13 @@ import random
 def get_bot_response(user_response):
    
 
-#add monthly practice by week
- bot_response_week_1 = ["Downward dog for 20 minutes", "Forward bend sequence ending in parchimotanasana with these poses we hal the nervous system.", "1 hour long savasana, nothing better for the mind."] 
- bot_response_week_2 = ["Start with a strong standing pose sequence", "Back-bend sequence on a chair" ,"Upward dog on a rope wall! Be  sure to work on arms before starting."]
+#add monthly practice by week and give suggestions on what todo!
+ bot_response_week_1 = ["Upwnward dog for 20 minutes, don't hold your breath!", "Forward bend sequence ending in parchimotanasana. These poses heal the nervous system.", "1 hour long savasana, nothing better for the mind."] 
+ bot_response_week_2 = ["Start with a strong standing pose sequence", "Back-bend sequence on a chair" ,"Upward dog on a rope wall! Be sure to work on arms before starting."]
  bot_response_week_3 = ["10 sun salutations to get you started", "Tree pose for balance! Do all balancing poses you can remeber", "Incorporate twists today!"]
  bot_response_week_4 = ["Hand stand sequence", "Shoulder stand and forearm balance! Yay it will be so fun!", "Hang on a sling for a few mintes and realign the spinal column!"]
 
-#more than one elif statement; each statement representing a possible input for the week of the month
+# i will need more than one elif if the user inputs an anwer what will the response be? choose from the lists
  if user_response == "forward bends":
     return choice(bot_response_week_1)
  elif user_response == "back bends":
